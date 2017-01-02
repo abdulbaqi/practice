ab740220bc8eb77b0a6d5db7aa0f936a40f9e09b
abdulbaqi <sharaf.al.islam@gmail.com>
2017-01-02 15:07:36 +0300

    1

---
+++ intermediateR.R
@@ -0,0 +1,17 @@
+# these practics are from DataCamp intermediate course
+# on R
+
+"hello" > "Goodbye"
+# the above is true because R does comparision alphabatically
+
+# Comparison of logicals
+T == F
+
+# Comparison of numerics
+(-6 * 14) != (17- 101)
+
+# Comparison of character strings
+"useR" == "user"
+
+# Compare a logical with a numeric
+T == 1
